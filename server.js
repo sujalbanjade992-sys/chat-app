a371e6f66c8690febc8ce2d7c342dd77386a3e37
sujalbanjade992-sys <sujalbanjade992@gmail.com>
2026-04-24 17:44:12 +0400

    SUJAL NETWORKS: ENCRYPTED CHAT

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
+const express = require('express');
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user joined Sujal Networks');
+
+  // Handle sending messages
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  // Handle the "is typing..." signal
+  socket.on('typing', (data) => {
+    // broadcast sends it to everyone EXCEPT the person typing
+    socket.broadcast.emit('display typing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user left Sujal Networks');
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
