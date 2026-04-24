9a0698ce81f1acca69fd671128231cd5726c52c6
sujalbanjade992-sys <sujalbanjade992@gmail.com>
2026-04-24 18:12:54 +0400

    SUJAL NETWORKS: ENCRYPTED CHAT

--- server.js
+++ server.js
@@ -10,28 +10,44 @@ const users = {};
 
 io.on('connection', (socket) => {
     socket.on('login', (username) => {
-        users[socket.id] = username;
+        // Assign a random avatar based on their name
+        const avatar = `https://api.dicebear.com/7.x/bottts/svg?seed=${username}`;
+        users[socket.id] = { name: username, avatar: avatar };
+        
         io.emit('user list', users); 
+        // Notify global chat
+        io.emit('chat message', { 
+            user: 'SYSTEM', 
+            text: `${username} joined the network.`, 
+            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+            isSystem: true 
+        });
     });
 
-    // PUBLIC MESSAGE (To Everyone)
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
 
-    // PRIVATE MESSAGE (To One Person)
     socket.on('private message', (data) => {
         io.to(data.toId).emit('private message', {
             fromId: socket.id,
-            fromName: users[socket.id],
+            fromName: users[socket.id].name,
+            fromAvatar: users[socket.id].avatar,
             text: data.text,
             time: data.time
         });
     });
 
     socket.on('disconnect', () => {
-        delete users[socket.id];
-        io.emit('user list', users);
+        if(users[socket.id]) {
+            io.emit('chat message', { 
+                user: 'SYSTEM', 
+                text: `${users[socket.id].name} left the network.`, 
+                isSystem: true 
+            });
+            delete users[socket.id];
+            io.emit('user list', users);
+        }
     });
 });
 
