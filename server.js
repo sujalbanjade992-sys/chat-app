c2873e79994f07cb7394740e2aa04010830fdb32
sujalbanjade992-sys <sujalbanjade992@gmail.com>
2026-04-24 08:03:27 +0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
-  console.log("Server running...");
+  console.log(`Server running on port ${PORT}`);
 });
